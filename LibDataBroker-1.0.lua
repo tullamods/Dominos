4718396dc8a209d644375d7f25a718ae91a2c585
elkano <elkano@48e1f902-e210-0410-a839-a0138e9d5bd5>
2008-03-02 12:21:17 +0000

    LibDataBroker-1.0: initial push to SVN
    git-svn-id: http://svn.wowace.com/wowace/branches/LibDataBroker-1.0/Elkano/LibDataBroker-1.0@63372 48e1f902-e210-0410-a839-a0138e9d5bd5

---
+++ LibDataBroker-1.0.lua
@@ -0,0 +1,73 @@
+--[[
+Name: LibDataBroker-1.0
+Revision: $Revision: 0 $
+Author: Elkano (elkano@gmx.de)
+Website: http://
+Documentation: http://www.wowace.com/wiki/LibDataBroker-1.0
+SVN: http://svn.wowace.com/wowace/trunk/LibDataBroker-1.0/
+Description: A central registry for addons looking for something to display their data.
+Dependencies: LibStub, CallbackHandler-1.0
+]]
+
+local MAJOR, MINOR = "LibDataBroker-1.0", "$Revision: 0 $"
+local lib = LibStub:NewLibrary(MAJOR, MINOR)
+
+if not lib then return end
+
+local _G = getfenv(0)
+local setmetatable = _G.setmetatable
+
+local CallbackHandler = LibStub:GetLibrary("CallbackHandler-1.0")
+
+lib.callbacks			= lib.callbacks			or CallbackHandler:New(lib)
+
+lib.attributestorage	= lib.attributestorage	or {}
+lib.namestorage			= lib.namestorage		or {}
+lib.proxystorage		= lib.proxystorage		or {}
+
+local attributestorage	= lib.attributestorage
+local namestorage		= lib.namestorage
+local proxystorage		= lib.proxystorage
+
+local domt
+domt = {
+	__metatable =		"access denied",
+	__newindex =		function(self, key, value)
+							if not attributestorage[self] then attributestorage[self] = {} end
+							attributestorage[self][key] = value
+							local name = namestorage[self]
+							if name then
+								lib.callbacks:Fire("LibDataBroker_AttributeChanged_"..name, name, key, value)
+							end
+						end,
+	__index =			function(self, key)
+							return attributestorage[self] and attributestorage[self][key] or nil
+						end,
+}
+
+function lib:NewDataObject()
+	local dataobject = setmetatable({}, domt)
+	return dataobject
+end
+
+function lib:RegisterDataObject(dataobjectname, dataobject)
+	if not proxystorage[dataobjectname] then
+		proxystorage[dataobjectname] = dataobject
+		namestorage[dataobject] = dataobjectname
+		lib.callbacks:Fire("LibDataBroker_DataobjectRegistered", dataobjectname, dataobject)
+		return true
+	end
+	return false
+end
+
+function lib:DataObjectIterator()
+	return pairs(proxystorage)
+end
+
+function lib:GetDataObjectByName(dataobjectname)
+	return proxystorage[dataobjectname]
+end
+
+function lib:GetNameByDataObject(dataobject)
+	return namestorage[dataobject]
+end
