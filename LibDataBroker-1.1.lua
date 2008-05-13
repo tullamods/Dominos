f9d04aca94f28ed089dc827bf4fd35921c2ea6d9
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-05-12 23:11:34 -0600

    Add asserts to ensure our embedder has provided LibStub and CBH-1.0

--- LibDataBroker-1.1.lua
+++ LibDataBroker-1.1.lua
@@ -1,4 +1,7 @@
 
+assert(LibStub, "LibDataBroker-1.1 requires LibStub")
+assert(LibStub:GetLibrary("CallbackHandler-1.0", true), "LibDataBroker-1.1 requires CallbackHandler-1.0")
+
 local lib = LibStub:NewLibrary("LibDataBroker-1.1", 1)
 if not lib then return end
 
