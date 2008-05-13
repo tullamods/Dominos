77693f3878bf1a6c5dd8340746786bbc726398a4
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-05-12 21:08:40 -0600

    Clean up redundant code, see redundant

--- Dominos/LibDataBroker-1.1/LibDataBroker-1.1.lua
+++ Dominos/LibDataBroker-1.1/LibDataBroker-1.1.lua
@@ -1,15 +1,9 @@
 
-local MAJOR, MINOR = "LibDataBroker-1.1", "$Revision$"
-local lib = LibStub:NewLibrary(MAJOR, MINOR)
-
+local lib = LibStub:NewLibrary("LibDataBroker-1.1", 1)
 if not lib then return end
 
-local _G = getfenv(0)
-local setmetatable = _G.setmetatable
 
-local CallbackHandler = LibStub:GetLibrary("CallbackHandler-1.0")
-
-lib.callbacks = lib.callbacks or CallbackHandler:New(lib)
+lib.callbacks = lib.callbacks or LibStub:GetLibrary("CallbackHandler-1.0"):New(lib)
 
 lib.attributestorage = lib.attributestorage or {}
 lib.namestorage = lib.namestorage or {}
@@ -19,26 +13,23 @@ local attributestorage = lib.attributestorage
 local namestorage = lib.namestorage
 local proxystorage = lib.proxystorage
 
-local domt
-domt = {
+local domt = {
 	__metatable = "access denied",
 	__newindex = function(self, key, value)
 		if not attributestorage[self] then attributestorage[self] = {} end
 		if attributestorage[self][key] == value then return end
 		attributestorage[self][key] = value
 		local name = namestorage[self]
-		if name then
-			lib.callbacks:Fire("LibDataBroker_AttributeChanged_"..name, name, key, value)
-		end
+		if not name then return end
+		lib.callbacks:Fire("LibDataBroker_AttributeChanged_"..name, name, key, value)
 	end,
 	__index = function(self, key)
-		return attributestorage[self] and attributestorage[self][key] or nil
+		return attributestorage[self] and attributestorage[self][key]
 	end,
 }
 
 function lib:NewDataObject()
-	local dataobject = setmetatable({}, domt)
-	return dataobject
+	return setmetatable({}, domt)
 end
 
 function lib:RegisterDataObject(dataobjectname, dataobject)
