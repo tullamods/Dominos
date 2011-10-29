bc107e1e162681ee13ac88fc44b41d84af869e2e
Tuller <tuller@tullamods.com>
2011-10-29 17:48:53 -0400

    updated toc for 4.3

---
+++ Dominos/actionButton.lua
@@ -0,0 +1,174 @@
+--[[ 
+	Action Button.lua
+		A dominos action button
+--]]
+
+local HiddenActionButtonFrame = CreateFrame('Frame');  HiddenActionButtonFrame:Hide() 
+
+local KeyBound = LibStub('LibKeyBound-1.0')
+
+local ActionButton = Dominos:CreateClass('CheckButton', Dominos.BindableButton)
+Dominos.ActionButton = ActionButton
+ActionButton.unused = {}
+ActionButton.active = {}
+
+--constructor
+function ActionButton:New(id)
+	local b = self:Restore(id) or self:Create(id)
+	if b then
+		b:SetAttribute('showgrid', 0)
+		b:SetAttribute('action--base', id)
+		b:SetAttribute('_childupdate-action', [[
+			local id = message and self:GetAttribute('action--' .. message) or self:GetAttribute('action--base')
+			self:SetAttribute('action', id)
+		]])
+
+		b:UpdateGrid()
+		b:UpdateHotkey(b.buttonType)
+		b:UpdateMacro()
+		b:UnregisterEvent('UPDATE_BINDINGS')
+
+		--hack #1billion, get rid of range indicator text
+		local hotkey = _G[b:GetName() .. 'HotKey']
+		if hotkey:GetText() == _G['RANGE_INDICATOR'] then
+			hotkey:SetText('')
+		end
+
+		self.active[id] = b
+
+		return b
+	end
+end
+
+local function Create(id)
+	if id <= 12 then
+		local b = _G['ActionButton' .. id]
+		b.buttonType = 'ACTIONBUTTON'
+		return b
+	elseif id <= 24 then
+		return CreateFrame('CheckButton', 'DominosActionButton' .. (id-12), nil, 'ActionBarButtonTemplate')
+	elseif id <= 36 then
+		return _G['MultiBarRightButton' .. (id-24)]
+	elseif id <= 48 then
+		return _G['MultiBarLeftButton' .. (id-36)]
+	elseif id <= 60 then
+		return _G['MultiBarBottomRightButton' .. (id-48)]
+	elseif id <= 72 then
+		return _G['MultiBarBottomLeftButton' .. (id-60)]
+	end
+	return CreateFrame('CheckButton', 'DominosActionButton' .. (id-60), nil, 'ActionBarButtonTemplate')
+end
+
+function ActionButton:Create(id)
+	local b = Create(id)
+	if b then
+		self:Bind(b)
+
+		--this is used to preserve the button's old id
+		--we cannot simply keep a button's id at > 0 or blizzard code will take control of paging
+		--but we need the button's id for the old bindings system
+		b:SetAttribute('bindingid', b:GetID())
+		b:SetID(0)
+
+		b:ClearAllPoints()
+		b:SetAttribute('useparent-actionpage', nil)
+		b:SetAttribute('useparent-unit', true)
+		b:EnableMouseWheel(true)
+		b:SetScript('OnEnter', self.OnEnter)
+		b:Skin()
+	end
+	return b
+end
+
+function ActionButton:Restore(id)
+	local b = self.unused[id]
+	if b then
+		self.unused[id] = nil
+		b:LoadEvents()
+		ActionButton_UpdateAction(b)
+		b:Show()
+		self.active[id] = b
+		return b
+	end
+end
+
+--destructor
+function ActionButton:Free()
+	local id = self:GetAttribute('action--base')
+
+	self.active[id] = nil
+	
+	-- for i, frame in pairs(ActionBarButtonEventsFrame.frames) do
+		-- if frame == self then
+			-- table.remove(ActionBarButtonEventsFrame.frames, i)
+			-- break
+		-- end
+	-- end
+
+	ActionBarActionEventsFrame_UnregisterFrame(self)
+	
+	self:SetParent(HiddenActionButtonFrame)
+	self:Hide()
+	self.action = nil
+
+	self.unused[id] = self
+end
+
+--these are all events that are registered OnLoad for action buttons
+function ActionButton:LoadEvents()
+	ActionBarActionEventsFrame_RegisterFrame(self)
+end
+
+--keybound support
+function ActionButton:OnEnter()
+	if Dominos:ShouldShowTooltips() then
+		ActionButton_SetTooltip(self)
+		ActionBarButtonEventsFrame.tooltipOwner = self
+		ActionButton_UpdateFlyout(self)
+	end
+	KeyBound:Set(self)
+end
+
+--override the old update hotkeys function
+hooksecurefunc('ActionButton_UpdateHotkeys', ActionButton.UpdateHotkey)
+
+--button visibility
+function ActionButton:UpdateGrid()
+	if self:GetAttribute('showgrid') > 0 then
+		ActionButton_ShowGrid(self)
+	else
+		ActionButton_HideGrid(self)
+	end
+end
+
+--macro text
+function ActionButton:UpdateMacro()
+	if Dominos:ShowMacroText() then
+		_G[self:GetName() .. 'Name']:Show()
+	else
+		_G[self:GetName() .. 'Name']:Hide()
+	end
+end
+
+function ActionButton:SetFlyoutDirection(direction)
+	self:SetAttribute('flyoutDirection', direction)
+	ActionButton_UpdateFlyout(self)
+end
+
+--utility function, resyncs the button's current action, modified by state
+function ActionButton:LoadAction()
+	local state = self:GetParent():GetAttribute('state-page')
+	local id = state and self:GetAttribute('action--' .. state) or self:GetAttribute('action--base')
+	self:SetAttribute('action', id)
+end
+
+function ActionButton:Skin()
+	if not Dominos:Masque('Action Bar', self) then
+		_G[self:GetName() .. 'Icon']:SetTexCoord(0.06, 0.94, 0.06, 0.94)
+		self:GetNormalTexture():SetVertexColor(1, 1, 1, 0.5)
+		
+		if _G[self:GetName() .. 'FloatingBG'] then
+			_G[self:GetName() .. 'FloatingBG']:Hide()
+		end
+	end
+end
