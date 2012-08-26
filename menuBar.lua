a6f4b8040a93783b348a6b5591ea372cd3c9d1e7
Jason Greer <tuller@tullamods.com>
2012-08-26 16:16:16 -0400

    Finished up menu bar layout
    Added support for disabling buttons
    Added back advanced layout support

--- menuBar.lua
+++ menuBar.lua
@@ -8,7 +8,7 @@ Dominos.MenuBar = MenuBar
 local WIDTH_OFFSET = 2
 local HEIGHT_OFFSET = 20
 
-local MENU_BUTTON_NAMES = {
+local MENU_BUTTON_DISPLAY_NAMES = {
 	[CharacterMicroButton] = CHARACTER_BUTTON,
 	[SpellbookMicroButton] = SPELLBOOK_ABILITIES_BUTTON,
 	[TalentMicroButton] = TALENTS_BUTTON,
@@ -22,12 +22,6 @@ local MENU_BUTTON_NAMES = {
 	[MainMenuMicroButton] = MAINMENU_BUTTON,
 	[HelpMicroButton] = HELP_BUTTON
 }
-
--- local oMoveMicroButtons = _G['MoveMicroButtons']
--- MoveMicroButtons = function(...)
-	-- print(
-	-- oMoveMicroButtons(...)
--- end
 
 --[[ Menu Bar ]]--
 
@@ -55,6 +49,10 @@ function MenuBar:Create(frameId)
 		PADH_OFFSET = 0
 		HEIGHT_OFFSET = 22
 		WIDTH_OFFSET = 0
+		
+		myButtons = table.new()
+		disabledButtons = table.new()
+		activeButtons = table.new()
 	]])
 	
 	header:SetFrameRef('OverrideActionBar', overrideActionBar)
@@ -70,8 +68,6 @@ function MenuBar:Create(frameId)
 	]])
 	
 	header:SetAttribute('layout-normal', [[ 
-		if not myButtons then return end
-		
 		for i, button in pairs(myButtons) do
 			button:SetParent(self)
 		end
@@ -81,8 +77,6 @@ function MenuBar:Create(frameId)
 	]])
 	
 	header:SetAttribute('layout-petbattle', [[ 
-		if not(myButtons) then return end
-		
 		local numButtons = #myButtons
 		local cols = ceil(numButtons / 2)
 		
@@ -102,8 +96,6 @@ function MenuBar:Create(frameId)
 	]])
 	
 	header:SetAttribute('layout-override', [[
-		if not(myButtons) then return end
-		
 		local numButtons = #myButtons
 		local cols = ceil(numButtons / 2)
 		local spacing = -2
@@ -141,8 +133,6 @@ function MenuBar:Create(frameId)
 		end
 	]])
 	
-	header:SetAttribute('_onstate-numButtons', [[ needsLayout = true ]])
-
 	header:SetAttribute('_onstate-columns', [[ needsLayout = true ]])
 	
 	header:SetAttribute('_onstate-spacing', [[ needsLayout = true ]])
@@ -151,52 +141,96 @@ function MenuBar:Create(frameId)
 	
 	header:SetAttribute('_onstate-padH', [[ needsLayout = true ]])
 	
+	header:SetAttribute('_onstate-topToBottom', [[ needsLayout = true ]])
+	
+	header:SetAttribute('_onstate-leftToRight', [[ needsLayout = true ]])
+	
+	header:SetAttribute('_onstate-disableButton', [[		
+		local button = self:GetFrameRef('buttonToDisable')
+		local disable = newstate
+
+		if disabledButtons[button] ~= disable then
+			disabledButtons[button] = disable		
+			needsLayout = true 
+		end
+	]])
+	
 	--add button method
 	header:SetAttribute('addButton', [[
 		local button = self:GetFrameRef('addButton')		
 		
 		if button then
-			myButtons = myButtons or table.new()
 			table.insert(myButtons, button)
 			button:SetParent(self:GetFrameRef('buttonFrame') or self)
 		end
+	]])
+	
+	header:SetAttribute('updateActiveButtons', [[
+		table.wipe(activeButtons)
 		
-		self:SetAttribute('maxLength', #myButtons)
+		for i = 1, #myButtons do
+			local button = myButtons[i]
+			
+			if not disabledButtons[button] then
+				table.insert(activeButtons, button)
+			end
+		end
+		
+		self:SetAttribute('maxLength', #activeButtons)
 	]])
 	
 	header:SetAttribute('layout', [[
-		if not(myButtons and needsLayout) then return end
-
-		local numButtons = self:GetAttribute('state-numButtons') or #myButtons
-		local cols = min(self:GetAttribute('state-columns') or numButtons, numButtons)
+		if not needsLayout then return end
 		
+		self:RunAttribute('updateActiveButtons')
+		
+		local numButtons = #activeButtons
+		if numButtons == 0 then
+			self:GetParent():SetWidth(36)
+			self:GetParent():SetHeight(36)
+			return
+		end
+		
+		local cols = min(self:GetAttribute('state-columns') or numButtons, numButtons)
 		local rows = ceil(numButtons / cols)
-		local spacing = self:GetAttribute('state-spacing') + SPACING_OFFSET
-		local pW = self:GetAttribute('state-padW') + PADW_OFFSET
-		local pH = self:GetAttribute('state-padH') + PADH_OFFSET
+
+		local pW, pH = self:GetAttribute('state-padW') or 0, self:GetAttribute('state-padH') or 0
+		local spacing = self:GetAttribute('state-spacing') or 0
+
+		local isLeftToRight = self:GetAttribute('state-leftToRight')
+		local isTopToBottom = self:GetAttribute('state-topToBottom')
 
 		local b = myButtons[1]
 		local w = b:GetWidth() + spacing - WIDTH_OFFSET
 		local h = b:GetHeight() + spacing - HEIGHT_OFFSET
 		
-		for i = numButtons + 1, #myButtons do
+		for i = 1, #myButtons do
 			myButtons[i]:Hide()
 		end
-		
-		self:GetParent():SetWidth(max(w*cols - spacing + pW*2 + WIDTH_OFFSET, 8))
-		self:GetParent():SetHeight(max(h*ceil(numButtons/cols) - spacing + pH*2, 8))
 
-		if numButtons > 0 then
-			for i = 1, numButtons do
-				local col = (i-1) % cols
-				local row = ceil(i / cols) - 1
+		for i = 1, numButtons do
+			local col, row
 			
-				local b = myButtons[i]
-				b:ClearAllPoints()
-				b:SetPoint('TOPLEFT', self, 'TOPLEFT', w*col + pW + WIDTH_OFFSET, -(h*row + pH) + HEIGHT_OFFSET)
-				b:Show()
+			if isLeftToRight then
+				col = (i-1) % cols
+			else
+				col = (cols-1) - (i-1) % cols
 			end
+
+			if isTopToBottom then
+				row = ceil(i / cols) - 1
+			else
+				row = rows - ceil(i / cols)
+			end
+			
+			local b = activeButtons[i]
+			b:ClearAllPoints()
+			b:SetPoint('TOPLEFT', '$parent', 'TOPLEFT', w*col + pW, -(h*row + pH) + HEIGHT_OFFSET)
+			b:Show()
 		end
+
+		self:GetParent():SetWidth(max(w*cols - spacing + pW*2 + WIDTH_OFFSET, 8))
+		self:GetParent():SetHeight(max(h*ceil(numButtons / cols) - spacing + pH*2, 8))
 		
 		needsLayout = nil
 	]])
@@ -237,13 +271,18 @@ function MenuBar:LoadSettings(...)
 	
 	local header = self.header
 
-	header:SetAttribute('state-numButtons', self:NumButtons())
 	header:SetAttribute('state-columns', self:NumColumns())
 	header:SetAttribute('state-spacing', self:GetSpacing())
+	header:SetAttribute('state-leftToRight', self:GetLeftToRight())
+	header:SetAttribute('state-topToBottom', self:GetTopToBottom())
 	
 	local pw, ph = self:GetPadding()
 	header:SetAttribute('state-padW', pw)
 	header:SetAttribute('state-padH', ph)
+	
+	for i, button in ipairs(self.buttons) do
+		self:DisableMenuButton(button, self:IsMenuButtonDisabled(button))
+	end
 end
 
 function MenuBar:GetDefaults()
@@ -259,19 +298,18 @@ function MenuBar:MaxLength()
 end
 
 function MenuBar:AddButton(button)
+	if not self.buttons then
+		self.buttons = {button}
+	else
+		table.insert(self.buttons, button)
+	end
+	
 	self.header:SetFrameRef('addButton', button)
 	self.header:Execute([[ self:RunAttribute('addButton') ]])
 end
 
-function MenuBar:SetNumButtons(numButtons)
-	self.sets.numButtons = numButtons or false
-	
-	self.header:SetAttribute('state-numButtons', numButtons or false) --here, false implies (use whatever the maximum value would be)
-	self:Layout()
-end
-
 function MenuBar:NumButtons()
-	return self.sets.numButtons or self:MaxLength()
+	return self:MaxLength()
 end
 
 function MenuBar:SetColumns(columns)
@@ -288,6 +326,22 @@ function MenuBar:SetSpacing(spacing)
 	self:Layout()
 end
 
+function MenuBar:SetLeftToRight(isLeftToRight)
+	local isRightToLeft = not isLeftToRight
+
+	self.sets.isRightToLeft = isRightToLeft
+	self.header:SetAttribute('state-leftToRight', isLeftToRight and true or false)
+	self:Layout()
+end
+
+function MenuBar:SetTopToBottom(isTopToBottom)
+	local isBottomToTop = not isTopToBottom
+
+	self.sets.isBottomToTop = isBottomToTop
+	self.header:SetAttribute('state-topToBottom', isTopToBottom and true or false)
+	self:Layout()
+end
+
 function MenuBar:SetPadding(padW, padH)
 	local padW = padW or 0
 	local padH = padH or padW
@@ -300,82 +354,87 @@ function MenuBar:SetPadding(padW, padH)
 	self:Layout()
 end
 
+function MenuBar:DisableMenuButton(button, disabled)
+	local disabledButtons = self.sets.disabled or {}
+
+	disabledButtons[button:GetName()] = disabled or false
+	self.header:SetFrameRef('buttonToDisable', button) 
+	self.header:SetAttribute('state-disableButton', disabled)
+	self:Layout()	
+	
+	self.sets.disabled = disabledButtons
+end
+
+function MenuBar:IsMenuButtonDisabled(button)
+	local disabledButtons = self.sets.disabled
+	
+	if disabledButtons then
+		return disabledButtons[button:GetName()]
+	end
+	
+	return false
+end
+
 function MenuBar:Layout(force)
 	if force then
 		self.header:Execute([[ needsLayout = true ]])
 	end
 	
-	self.header:Execute([[ 
-		self:RunAttribute('layout') 
-	]])
+	self.header:Execute([[ self:RunAttribute('layout') ]])
 end
 
 --[[ Menu Code ]]--
 
-local function SizeSlider_OnShow(self)
-	local owner = self:GetParent().owner
-	local minValue = 1
-	local maxValue = owner:MaxLength()
-	local currentValue = owner:NumButtons()
+local function Menu_AddLayoutPanel(menu)
+	local panel = menu:NewPanel(LibStub('AceLocale-3.0'):GetLocale('Dominos-Config').Layout)
 	
-	self:SetMinMaxValues(minValue, maxValue)
-	self:SetValue(currentValue)
+	panel:NewOpacitySlider()
+	panel:NewFadeSlider()
+	panel:NewScaleSlider()
+	panel:NewPaddingSlider()
+	panel:NewSpacingSlider()
+	panel:NewColumnsSlider()
+	
+	return panel
 end
 
-local function SizeSlider_UpdateValue(self, value)
-	local owner = self:GetParent().owner
-	
-	owner:SetNumButtons(value)
-	
-	self:GetParent().Cols:OnShow()
-end
-	
-local function panel_AddSizeSlider(panel)
-	local name = LibStub('AceLocale-3.0'):GetLocale('Dominos-Config').Size
-	
-	return panel:NewSlider(name, 1, 1, 1, SizeSlider_OnShow, SizeSlider_UpdateValue)
-end
+local function Panel_AddDisableMenuButtonCheckbox(panel, button)
+	local checkbox = panel:NewCheckButton(MENU_BUTTON_DISPLAY_NAMES[button])
 
-local function AddLayoutPanel(menu)
-	local p = menu:NewPanel(LibStub('AceLocale-3.0'):GetLocale('Dominos-Config').Layout)
-	p:NewOpacitySlider()
-	p:NewFadeSlider()
-	p:NewScaleSlider()
-	p:NewPaddingSlider()
-	p:NewSpacingSlider()
-	p.Cols = p:NewColumnsSlider()
-
-	panel_AddSizeSlider(p)
-end
-
-local function NewCheckButton(name, button, p)
-	local tog = p:NewCheckButton(name)
-
-	tog:SetScript('OnClick', function(self)
-		self:GetParent().owner.sets.disabled[button:GetName()] = self:GetChecked() or nil
-		self:GetParent().owner:Layout()
+	checkbox:SetScript('OnClick', function(self)
+		local owner = self:GetParent().owner
+		
+		owner:DisableMenuButton(button, self:GetChecked())
 	end)
 
-	tog:SetScript('OnShow', function(self)
-		self:SetChecked(self:GetParent().owner.sets.disabled[button:GetName()])
+	checkbox:SetScript('OnShow', function(self)
+		local owner = self:GetParent().owner
+		
+		self:SetChecked(owner:IsMenuButtonDisabled(button))
 	end)
 
-	return tog
+	return checkbox
 end
 
-local function AddDisableButtonPanel(menu)
-	local p = menu:NewPanel(LibStub('AceLocale-3.0'):GetLocale('Dominos-Config').DisableMenuButtons)
-	for i = 1, MenuBar:NumMenuButtons() do
-		local button = MenuBar:GetMenuButton(i)
-		NewCheckButton(MENU_BUTTON_NAMES[button] or  button:GetName(), button, p)
+local function Menu_AddDisableMenuButtonsPanel(menu)
+	local panel = menu:NewPanel(LibStub('AceLocale-3.0'):GetLocale('Dominos-Config').DisableMenuButtons)
+	panel.width = 200
+	
+	for button in pairs(MENU_BUTTON_DISPLAY_NAMES) do
+		Panel_AddDisableMenuButtonCheckbox(panel, button)
 	end
+	
+	return panel
 end
 
 function MenuBar:CreateMenu()
 	local menu = Dominos:NewMenu(self.id)
 
-	AddLayoutPanel(menu)
-	-- AddDisableButtonPanel(menu)
+	Menu_AddLayoutPanel(menu)
+	Menu_AddDisableMenuButtonsPanel(menu)
 	menu:AddAdvancedPanel()
+	
 	self.menu = menu
+	
+	return menu
 end
