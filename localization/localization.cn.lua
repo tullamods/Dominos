5b131d963d4b00581f518995589169fe20fffda4
yaroot <yaroot@gmail.com>
2012-09-05 17:40:49 +0800

    update zhCN/zhTW

--- localization/localization.cn.lua
+++ localization/localization.cn.lua
@@ -1,25 +1,24 @@
 ﻿--[[
 	Localization_zhCN.lua
 	2008/12/4 Modified by xuxianhe@gmail.com
-
-	Simple Chinese : 简体中文
+    Sep 9. 2012     yaroot##gmail#com
 --]]
 
 local L = LibStub('AceLocale-3.0'):NewLocale('Dominos', 'zhCN')
 if not L then return end
 
 --system messages
-L.NewPlayer = '建立新配置 %s'
+L.NewPlayer = '建立新角色方案 %s'
 L.Updated = '升级到 v%s'
 
 --profiles
-L.ProfileCreated = '建立新配置 "%s"'
-L.ProfileLoaded = '配置设置为 "%s"'
-L.ProfileDeleted = '删除配置 "%s"'
-L.ProfileCopied = '从 "%s" 复制配置到 "%s"'
-L.ProfileReset = '重置配置 "%s"'
-L.CantDeleteCurrentProfile = '不能删除当前配置'
-L.InvalidProfile = '无效的配置文件 "%s"'
+L.ProfileCreated = '建立新方案 "%s"'
+L.ProfileLoaded = '方案设置为 "%s"'
+L.ProfileDeleted = '删除方案 "%s"'
+L.ProfileCopied = '已复制方案"%s"的设置到当前方案'
+L.ProfileReset = '重置方案 "%s"'
+L.CantDeleteCurrentProfile = '不能删除当前在用的方案'
+L.InvalidProfile = '无效的方案或者已是当前方案 "%s"'
 
 --slash command help
 L.ShowOptionsDesc = '显示设置菜单'
@@ -38,13 +37,13 @@ L.HideFramesDesc = '隐藏 <frameList>'
 L.ToggleFramesDesc = '开关 <frameList>'
 
 --slash commands for profiles
-L.SetDesc = '配置切换为 <profile>'
-L.SaveDesc = '保存当前配置为 <profile>'
-L.CopyDesc = '从 <profile> 复制配置'
+L.SetDesc = '方案切换为 <profile>'
+L.SaveDesc = '保存当前方案为 <profile>'
+L.CopyDesc = '从 <profile> 复制设置'
 L.DeleteDesc = '删除 <profile>'
 L.ResetDesc = '返回默认配置'
-L.ListDesc = '列出所有配置'
-L.AvailableProfiles = '可用设置'
+L.ListDesc = '列出所有方案'
+L.AvailableProfiles = '可用方案'
 L.PrintVersionDesc = '显示当前版本'
 
 --dragFrame tooltips
@@ -52,6 +51,7 @@ L.ShowConfig = '<右键> 设置'
 L.HideBar = '<中键或者Shift+右键> 隐藏'
 L.ShowBar = '<中键或者Shift+右键> 显示'
 L.SetAlpha = '<滚轮> 设置透明度 (|cffffffff%d|r)'
+
 --minimap button stuff
 L.ConfigEnterTip = '<左键> 进入设置模式'
 L.ConfigExitTip = '<左键> 退出设置模式'
@@ -62,9 +62,10 @@ L.ShowOptionsTip = '<右键> 显示设置菜单'
 --helper dialog stuff
 L.ConfigMode = '设置模式'
 L.ConfigModeExit = '退出设置模式'
-L.ConfigModeHelp = '<拖动> 移动技能条.  <右键> 进入设置.  <中键> 或 <Shift+右键> 设置是否显示'
+L.ConfigModeHelp = '<拖动> 移动技能条(按住Alt键可以不粘附)。\n<右键> 进入设置。\n<中键> 或 <Shift+右键> 设置是否显示。'
+
 --bar tooltips
-L.TipRollBar = '在团队中时，显示物品掷点面板'
+L.TipRollBar = '在团队中显示物品掷点面板'
 L.TipVehicleBar = [[
-Displays controls to aim and exit a vehicle.
-All other vehicle actions are displayed on the possess bar.]]
+显示瞄准和离开载具的控制按钮.
+其他载具按钮将在心灵控制条上显示.]]
