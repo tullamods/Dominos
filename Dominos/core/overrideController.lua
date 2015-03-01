243441ae1ccb6b327c0b1ee8d6a2a55518993522
Jason Greer <tuller@tullamods.com>
2015-03-01 16:51:02 -0500

    whitespace mess

--- Dominos/core/overrideController.lua
+++ Dominos/core/overrideController.lua
@@ -12,78 +12,78 @@ local overrideBarStates = {
 }
 
 function OverrideController:Load()
- 	--[[ 
+ 	--[[
 		Override UI Detection
 	--]]
-	
+
 	local overrideUIWatcher = CreateFrame('Frame', nil, _G['OverrideActionBar'], 'SecureHandlerShowHideTemplate')
 	overrideUIWatcher:SetFrameRef('controller', self)
 	self.overrideUIWatcher = overrideUIWatcher
 
-	overrideUIWatcher:SetAttribute('_onshow', [[ 
+	overrideUIWatcher:SetAttribute('_onshow', [[
 		self:GetFrameRef('controller'):SetAttribute('state-isoverrideuishown', true)
 	]])
 
-	overrideUIWatcher:SetAttribute('_onhide', [[ 
+	overrideUIWatcher:SetAttribute('_onhide', [[
 		self:GetFrameRef('controller'):SetAttribute('state-isoverrideuishown', false)
 	]])
-	
-	
-	self:SetAttribute('_onstate-isoverrideuishown', [[ 
-		self:RunAttribute('updateOverrideUI') 
+
+
+	self:SetAttribute('_onstate-isoverrideuishown', [[
+		self:RunAttribute('updateOverrideUI')
 	]])
 
-	self:SetAttribute('_onstate-useoverrideui', [[ 
-		self:RunAttribute('updateOverrideUI') 
+	self:SetAttribute('_onstate-useoverrideui', [[
+		self:RunAttribute('updateOverrideUI')
 	]])
 
-	self:SetAttribute('_onstate-overrideui', [[	
+	self:SetAttribute('_onstate-overrideui', [[
 		for i, frame in pairs(myFrames) do
 			frame:SetAttribute('state-overrideui', newstate)
 		end
 	]])
-	
+
 	self:SetAttribute('updateOverrideUI', [[
 		local isOverrideUIVisible = self:GetAttribute('state-useoverrideui') and self:GetAttribute('state-isoverrideuishown')
-		
+
 		self:SetAttribute('state-overrideui', isOverrideUIVisible)
 	]])
 
 
-	--[[ 
-		Pet Battle UI Detection 
+	--[[
+		Pet Battle UI Detection
 	--]]
 
 	self:SetAttribute('_onstate-petbattleui', [[
 		local hasPetBattleUI = newstate == 1
-		
+
 		for i, frame in pairs(myFrames) do
 			frame:SetAttribute('state-petbattleui', hasPetBattleUI)
 		end
 	]])
-	
-	
+
+
 	--[[
 		Override Page State Detection
 	--]]
-	
-	self:SetAttribute('_onstate-overridepage', [[	
-		local overridePage = newstate or 0	
-		
+
+	self:SetAttribute('_onstate-overridepage', [[
+		local overridePage = newstate or 0
+
 		for i, frame in pairs(myFrames) do
 			frame:SetAttribute('state-overridepage', overridePage)
 		end
 	]])
-	
+
 	for state in pairs(overrideBarStates) do
-		self:SetAttribute('_onstate-' .. state, [[ 
+		self:SetAttribute('_onstate-' .. state, [[
 			self:RunAttribute('updateOverridePage')
 		]])
 	end
-	
+
 	self:SetAttribute('updateOverridePage', [[
 		local newPage = 0
-	
+
 		if HasVehicleActionBar() then
 			newPage = GetVehicleBarIndex() or 0
 		elseif HasOverrideActionBar() then
@@ -93,21 +93,21 @@ function OverrideController:Load()
 		else
 			newPage = GetBonusBarOffset() or 0
 		end
-		
+
 		self:SetAttribute('state-overridepage', newPage)
 	]])
-	
-	
+
+
 	--[[
 		Initialization
 	--]]
-	
+
 	self:Execute([[ myFrames = table.new() ]])
-	
+
 	self:SetAttribute('state-isoverrideuishown', self.overrideUIWatcher:IsVisible() and true or false)
-	
+
 	RegisterStateDriver(self, 'petbattleui', '[petbattle]1;0')
-		
+
 	for state, values in pairs(overrideBarStates) do
 		RegisterStateDriver(self, state, values)
 	end
@@ -115,12 +115,12 @@ end
 
 function OverrideController:Add(frame)
 	self:SetFrameRef('FrameToRegister', frame)
-	self:Execute([[ 
+	self:Execute([[
 		local frame = self:GetFrameRef('FrameToRegister')
-		
-		table.insert(myFrames, frame)		
+
+		table.insert(myFrames, frame)
 	]])
-	
+
 	--load states
 	frame:SetAttribute('state-overrideui', self:GetAttribute('state-overrideui'))
 	frame:SetAttribute('state-petbattleui', tonumber(self:GetAttribute('state-petbattleui')) == 1)
@@ -129,7 +129,7 @@ end
 
 function OverrideController:Remove(frame)
 	self:SetFrameRef('FrameToUnregister', frame)
-	self:Execute([[ 
+	self:Execute([[
 		local frameToUnregister = self:GetFrameRef('FrameToUnregister')
 		for i, frame in pairs(myFrames) do
 			if frame == frameToUnregister then
@@ -142,6 +142,7 @@ end
 
 function OverrideController:DumpStates()
 	Dominos:Print('Active States:')
+
 	print('------------------------------')
 	for state in pairs(overrideBarStates) do
 		local stateValue = self:GetAttribute('state-' .. state) == 1
@@ -149,6 +150,7 @@ function OverrideController:DumpStates()
 			print(state)
 		end
 	end
+
 	print('------------------------------')
 	print('effective actionpage', self:GetAttribute('state-overridepage'))
 end
