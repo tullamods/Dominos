2f25fdbe04be293e52c8d311bbe2b756cafe9f08
Jason Greer <tuller@tullamods.com>
2015-03-01 18:56:57 -0500

    Added support for cancelling flights
    Resolves issue  #193

--- Dominos/bars/vehicleBar.lua
+++ Dominos/bars/vehicleBar.lua
@@ -1,12 +1,38 @@
-local VehicleBar = Dominos:CreateClass('Frame', Dominos.ButtonBar)
+local Addon = _G[...]
+
+--[[ The Bar ]]--
+
+local VehicleBar = Addon:CreateClass('Frame', Addon.ButtonBar)
 
 function VehicleBar:New()
-	local f = VehicleBar.proto.New(self, 'vehicle')
+	local frame = VehicleBar.proto.New(self, 'vehicle')
 
-	f:LoadButtons()
-	f:Layout()
+	frame.header:SetAttribute('_onstate-taxi', [[
+		self:RunAttribute('updateVehicleButton')
+	]])
 
-	return f
+	frame.header:SetAttribute('_onstate-canexitvehicle', [[
+		self:RunAttribute('updateVehicleButton')
+	]])
+
+	frame.header:SetAttribute('updateVehicleButton', [[
+		local isVisible = self:GetAttribute('state-taxi') == 1
+		 				  or self:GetAttribute('state-canexitvehicle') == 1
+
+		self:SetAttribute('state-display', isVisible and 'show' or 'hide')
+	]])
+
+	RegisterStateDriver(frame.header, 'canexitvehicle', '[canexitvehicle]1;0')
+
+	frame:UpdateOnTaxi()
+	frame:LoadButtons()
+	frame:Layout()
+
+	return frame
+end
+
+function VehicleBar:UpdateOnTaxi()
+	self.header:SetAttribute('state-taxi', UnitOnTaxi('player') and 1 or 0)
 end
 
 function VehicleBar:GetDefaults()
@@ -18,31 +44,22 @@ function VehicleBar:GetDefaults()
 end
 
 function VehicleBar:GetShowStates()
-	return '[@vehicle,exists]show;hide'
+	return nil
 end
 
-function VehicleBar:NumButtons(f)
+function VehicleBar:NumButtons()
 	return 1
 end
 
-function VehicleBar:AddButton(i)
-	local b = self:GetLeaveButton()
-	b:UnregisterAllEvents()
-	if b then
-		b:SetParent(self.header)
-		b:Show()
+function VehicleBar:AddButton(index)
+	local button = self:GetLeaveButton()
+	button:UnregisterAllEvents()
 
-		self.buttons[i] = b
-	end
-end
+	if button then
+		button:SetParent(self.header)
+		button:Show()
 
-function VehicleBar:RemoveButton(i)
-	local b = self.buttons[i]
-	if b then
-		b:SetParent(nil)
-		b:Hide()
-
-		self.buttons[i] = nil
+		self.buttons[index] = button
 	end
 end
 
@@ -50,17 +67,33 @@ function VehicleBar:GetLeaveButton()
 	return _G['MainMenuBarVehicleLeaveButton']
 end
 
+
 --[[ Controller ]]--
 
-local VehicleBarController = Dominos:NewModule('VehicleBar')
+local VehicleBarController = Addon:NewModule('VehicleBar', 'AceEvent-3.0')
 
 function VehicleBarController:Load()
 	self.frame = VehicleBar:New()
+
+	self:RegisterEvent('UPDATE_BONUS_ACTIONBAR', 'UpdateOnTaxi')
+	self:RegisterEvent('UPDATE_MULTI_CAST_ACTIONBAR', 'UpdateOnTaxi')
+	self:RegisterEvent('UNIT_ENTERED_VEHICLE', 'UpdateOnTaxi')
+	self:RegisterEvent('UNIT_EXITED_VEHICLE', 'UpdateOnTaxi')
+	self:RegisterEvent('VEHICLE_UPDATE', 'UpdateOnTaxi')
+	self:RegisterEvent('PLAYER_REGEN_ENABLED', 'UpdateOnTaxi')
 end
 
 function VehicleBarController:Unload()
+	self:UnregisterAllEvents()
+
 	if self.frame then
 		self.frame:Free()
 		self.frame = nil
 	end
+end
+
+function VehicleBarController:UpdateOnTaxi()
+	if InCombatLockdown() then return end
+
+	self.frame:UpdateOnTaxi()
 end
