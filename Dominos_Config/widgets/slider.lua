8239fffe0388410baf12dbb0fe939cedcebb251d
Jason Greer <tuller@tullamods.com>
2016-07-08 23:14:17 -0400

    sliders with numeric edit boxes

--- Dominos_Config/widgets/slider.lua
+++ Dominos_Config/widgets/slider.lua
@@ -3,51 +3,122 @@ local Slider = Addon:CreateClass('Slider')
 
 do
 	local nextName = Addon:CreateNameGenerator('Slider')
-	
-	local getOrCall = function(self, v, ...) 
-		if type(v) == 'function' then 
+
+	local getOrCall = function(self, v, ...)
+		if type(v) == 'function' then
 			return v(self, ...)
 		end
 		return v
 	end
-		
+
+	local function editBox_OnEditFocusLost(self)
+		self:GetParent():UpdateText()
+	end
+
+	local function editBox_OnTextChanged(self)
+		local value = tonumber(self:GetText())
+
+		if value and value ~= self:GetParent():GetValue() then
+			local min, max = self:GetParent():GetMinMaxValues()
+
+			self:GetParent():SetValue(math.max(math.min(value, max), min))
+		end
+	end
+
+	local function editBox_OnEscapePressed(self)
+		self:ClearFocus()
+	end
+
+	local function editBox_FocusNext(self, ...)
+		local editBoxes = {}
+
+		local index = nil
+		for i = 1, select('#', ...) do
+			local vt = select(i, ...).valText
+			if vt and vt:GetObjectType() == 'EditBox' then
+				table.insert(editBoxes, vt)
+
+				if vt == self then
+					index = i
+				end
+			end
+		end
+
+		editBoxes[index % #editBoxes + 1]:SetFocus()
+	end
+
+	local function editBox_OnTabPressed(self)
+		editBox_FocusNext(self, self:GetParent():GetParent():GetChildren())
+
+
+		local value = self:GetNumber()
+		local min, max = self:GetParent():GetMinMaxValues()
+		local current = self:GetParent():GetValue()
+
+		if value and value ~= current and value >= min and value <= max then
+			self:GetParent():SetValue(math.max(math.min(value, max), min))
+		end
+	end
+
 	function Slider:New(options)
-		local f = self:Bind(CreateFrame('Slider', nextName(), options.parent, 'OptionsSliderTemplate'))	
-		
-		f.text = _G[f:GetName() .. 'Text']    	    
+		local f = self:Bind(CreateFrame('Slider', nextName(), options.parent, 'OptionsSliderTemplate'))
+
+		f.text = _G[f:GetName() .. 'Text']
 		f.text:SetFontObject('GameFontNormalLeft')
 		f.text:ClearAllPoints()
 		f.text:SetPoint('BOTTOMLEFT', f, 'TOPLEFT')
 		f.text:SetText(options.name or '')
-		
+
 		f.High:Hide()
 		f.Low:Hide()
 		f:EnableMouseWheel(true)
-				
+
 		f.min = options.min or 0
 		f.max = options.max or 100
-		f.step = options.step or 1	
-		f.GetSavedValue = options.get		
-		f.SetSavedValue = options.set			
-		f.FormatValue = options.format
-		
+		f.step = options.step or 1
+		f.GetSavedValue = options.get
+		f.SetSavedValue = options.set
+
 		--todo: add edit option
-		local text = f:CreateFontString(nil, 'BACKGROUND', 'GameFontHighlightSmall')
-		text:SetJustifyH('RIGHT')
-		text:SetPoint('BOTTOMRIGHT', f, 'TOPRIGHT')
-		f.valText = text			
-		
+		local editBox = CreateFrame('EditBox', nil, f)
+		editBox:SetPoint('BOTTOMRIGHT', f, 'TOPRIGHT')
+		editBox:SetNumeric(true)
+		editBox:SetAutoFocus(false)
+		editBox:SetFontObject('GameFontHighlightRight')
+		editBox:SetHeight(f.text:GetHeight())
+		editBox:SetWidth(f.text:GetHeight() * 3)
+		editBox:HighlightText(0, 0)
+		-- editBox:SetJustifyH('RIGHT')
+		-- editBox:SetText('')
+		editBox:SetScript('OnTextChanged', editBox_OnTextChanged)
+		editBox:SetScript('OnEditFocusLost', editBox_OnEditFocusLost)
+		editBox:SetScript('OnEscapePressed', editBox_OnEscapePressed)
+		editBox:SetScript('OnTabPressed', editBox_OnTabPressed)
+
+		local bg = editBox:CreateTexture(nil, 'BACKGROUND')
+		bg:SetAllPoints(bg:GetParent())
+		bg:SetColorTexture(0.2, 0.2, 0.2, 0.5)
+		editBox.bg = bg
+		f.valText = editBox
+
 		-- register Events
 		f:SetScript('OnShow', f.OnShow)
 		f:SetScript('OnValueChanged', f.OnValueChanged)
 		f:SetScript('OnMouseWheel', f.OnMouseWheel)
+		f:SetWidth(284)
 
 		return f
 	end
 
 	--[[ Frame Events ]]--
 
+	-- the render call here is a delay hack since it seemed to fix
+
 	function Slider:OnShow()
+		Addon:Render(self)
+	end
+
+	function Slider:OnRender()
 		self:UpdateRange()
 		self:UpdateValue()
 	end
@@ -58,11 +129,11 @@ do
 		local value = min + ceil((value - min) / step) * step
 
 		self:SetSavedValue(value)
-		self:UpdateText(self:GetSavedValue())
+		self:UpdateText(value)
 	end
 
 	function Slider:OnMouseWheel(direction)
-		local step = self:GetValueStep() *  direction
+		local step = self:GetValueStep() * direction
 		local value = self:GetValue()
 		local minVal, maxVal = self:GetMinMaxValues()
 
@@ -74,12 +145,12 @@ do
 	end
 
 	--[[ Update Methods ]]--
-	
+
 	function Slider:GetEffectiveSize()
 		local width, height = self:GetSize()
-		
+
 		height = height + self.text:GetHeight()
-		
+
 		return width, height
 	end
 
@@ -90,17 +161,17 @@ do
 	function Slider:GetSavedValue()
 		assert(false, 'Hey, you forgot to set GetSavedValue for ' .. self:GetName())
 	end
-	
+
 	function Slider:UpdateRange()
 		local min = getOrCall(self, self.min)
 		local max = getOrCall(self, self.max)
-		
+
 		local oldMin, oldMax = self:GetMinMaxValues()
 		if oldMin ~= min or oldMax ~= max then
 			self:SetEnabled(max > min)
 			self:SetMinMaxValues(min, max)
 		end
-		
+
 		local step = getOrCall(self, self.step)
 		if step ~= self:GetValueStep() then
 			self:SetValueStep(step)
@@ -111,25 +182,16 @@ do
 		local min, max = self:GetMinMaxValues()
 		self:SetEnabled(max > min)
 		self:SetValue(self:GetSavedValue())
-		self:UpdateText(self:GetSavedValue())
-	end
-	
-	function Slider:FormatValue(value)
-		return value
 	end
 
 	function Slider:UpdateText(value)
-		if type(self.FormatValue) == 'string' then
-			self.valText:SetFormattedText(self.FormatValue, value)
-		else
-			self.valText:SetText(self:FormatValue(value))
-		end
+		self.valText:SetText(value or self:GetSavedValue())
 	end
-	
+
 	function Slider:SetEnabled(enable)
 		if enable then
 			return BlizzardOptionsPanel_Slider_Enable(self)
-		end		
+		end
 		return BlizzardOptionsPanel_Slider_Disable(self)
 	end
 end
