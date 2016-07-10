629cbc1fb76c30a575c93fd5a8991bf7032eb80e
Jason Greer <tuller@tullamods.com>
2016-07-10 19:45:18 -0400

    add font selector

--- Dominos_Config/widgets/fontSelector.lua
+++ Dominos_Config/widgets/fontSelector.lua
@@ -4,24 +4,13 @@
 --]]
 
 local AddonName, Addon = ...
-local PADDING = 2
-local FONT_HEIGHT = 24
-local BUTTON_HEIGHT = 24
-local SCROLL_STEP = BUTTON_HEIGHT + PADDING
 
--- wrapper around LSM
+-- wrapper around LSM in case i want to replace it with something else
+-- in the future
 local Fonts = {}
 do
 	local libSharedMedia = LibStub('LibSharedMedia-3.0')
 	local key = libSharedMedia.MediaType.FONT
-
-	local fontTester = nil
-	local function isValidFont(font)
-		if not fontTester then
-			fontTester = CreateFont(AddonName .. '_FontTester')
-		end
-		return fontTester:SetFont(font, FONT_HEIGHT, 'OUTLINE')
-	end
 
 	function Fonts:Get(id)
 		if id and libSharedMedia:IsValid(key, id) then
@@ -37,196 +26,157 @@ do
 end
 
 --[[
-	The Font Button
+	The Texture Button
 --]]
 
 local FontButton = Addon:CreateClass('CheckButton')
 do
-	function FontButton:New(parent)
-		local b = self:Bind(CreateFrame('CheckButton', nil, parent))
-		b:SetHeight(BUTTON_HEIGHT)
-		b:SetScript('OnClick', b.OnClick)
-		b:SetScript('OnEnter', b.OnEnter)
-		b:SetScript('OnLeave', b.OnLeave)
+	local unused = {}
 
-		local bg = b:CreateTexture(nil, 'BACKGROUND')
-		bg:SetAllPoints(b)
-		b.bg = bg
-		b:OnLeave()
+	local function createButton(parent)
+		local button = CreateFrame('CheckButton', nil, parent)
 
-		local text = b:CreateFontString(nil, 'ARTWORK')
-		text:SetPoint('BOTTOM', 0, PADDING)
-
-		b:SetFontString(text)
-		b:SetNormalFontObject('GameFontNormalSmall')
-		b:SetHighlightFontObject('GameFontHighlightSmall')
-
-		local fontText = b:CreateFontString(nil, 'ARTWORK')
-		fontText:SetPoint('BOTTOM', text, 'TOP', 0, 2)
-		b.fontText = fontText
-
-		local ct = b:CreateTexture(nil, 'OVERLAY')
+		local ct = button:CreateTexture(nil, 'OVERLAY')
 		ct:SetTexture([[Interface\Buttons\UI-CheckBox-Check]])
-		ct:SetPoint('RIGHT', fontText, 'LEFT', -5, 0)
-		ct:SetSize(24, 24)
-		b:SetCheckedTexture(ct)
+		ct:SetPoint('LEFT')
+		ct:SetSize(16, 16)
+		button:SetCheckedTexture(ct)
 
-		return b
+		button:SetNormalFontObject('GameFontNormal')
+		button:SetHighlightFontObject('GameFontHighlight')
+		button:SetText('Loading...')
+		button:GetFontString():ClearAllPoints()
+		button:GetFontString():SetPoint('LEFT', ct, 'RIGHT', 2, 0)
+
+		return button
 	end
 
-	function FontButton:SetFontFace(font)
-		self.fontText:SetFont(font, FONT_HEIGHT, 'OUTLINE')
-		self.fontText:SetText('1234567890')
+	function FontButton:New(parent, owner)
+		local button = table.remove(unused)
 
-		return self
+		if button then
+			button:SetParent(parent)
+			button:Show()
+		else
+			button = self:Bind(createButton(parent))
+			button:SetScript('OnClick', self.OnClick)
+			-- button:SetScript('OnEnter', self.OnEnter)
+			-- button:SetScript('OnLeave', self.OnLeave)
+		end
+
+		button.owner = owner or parent
+		return button
 	end
 
-	function FontButton:GetFontFace()
-		return (self.fontText:GetFont())
+	function FontButton:Free()
+		self:ClearAllPoints()
+		self:SetParent(nil)
+		self:Hide()
+
+		table.insert(unused, self)
+	end
+
+	function FontButton:OnClick()
+		self.owner:OnSelectValue(self.value)
+		self:SetChecked(true)
 	end
 
 	function FontButton:OnEnter()
-		self.bg:SetColorTexture(1, 1, 1, 0.3)
+		self.bg:SetDesaturated(false)
 	end
 
 	function FontButton:OnLeave()
-		self.bg:SetColorTexture(0.25, 0.25, 0.25, 0.6)
+		self.bg:SetDesaturated(true)
+	end
+
+	function FontButton:SetValue(value, selectedValue)
+		local font = Fonts:Get(value)
+
+		self.value = value
+		self:SetChecked(value == selectedValue)
+		self:GetFontString():SetFont(font, 14)
+
+		self:SetText(value)
 	end
 end
 
---[[
-	The Font Selector
---]]
-
 local FontSelector = Addon:CreateClass('Frame')
 do
-	function FontSelector:New(title, parent, width, height)
-			local f = self:Bind(CreateFrame('Frame', nil, parent))
+	FontSelector.buttonWidth = 282
+	FontSelector.buttonHeight = 24
+	FontSelector.buttonPadding = 2
 
-			local scrollBar = f:CreateScrollBar()
-			scrollBar:SetPoint('TOPRIGHT', -4, -4)
-			scrollBar:SetPoint('BOTTOMRIGHT', -4, 4)
-			scrollBar:SetWidth(16)
-			f.scrollBar = scrollBar
+	function FontSelector:New(options)
+		local f = self:Bind(CreateFrame('Frame', nil, options.parent))
 
-			local scrollFrame = f:CreateScrollFrame()
-			scrollFrame:SetPoint('TOPLEFT', 4, -4)
-			scrollFrame:SetPoint('BOTTOMRIGHT', scrollBar, 'BOTTOMLEFT')
-			f.scrollFrame = scrollFrame
+		-- local bg = f:CreateTexture(nil, 'BACKGROUND')
+		-- bg:SetAllPoints(bg:GetParent())
+		-- bg:SetColorTexture(0.2, 0.8, 0.2, 0.5)
+		-- f.bg = bg
 
-			local scrollChild = f:CreateScrollChild()
-			f.scrollChild = scrollChild
-			scrollFrame:SetScrollChild(scrollChild)
+		f.SetSavedValue = options.set
+		f.GetSavedValue = options.get
+		f:SetScript('OnShow', self.OnShow)
+		f:SetScript('OnHide', self.OnHide)
+		f.buttons = {}
+		f:SetAllPoints(options.parent)
 
-
-			f:SetScript('OnShow', f.OnShow)
 		return f
 	end
 
-	do
-		local function scrollFrame_OnSizeChanged(self)
-			local scrollChild = self:GetParent().scrollChild
-			scrollChild:SetWidth(self:GetWidth())
-
-			local scrollBar  = self:GetParent().scrollBar
-			local scrollMax = max(scrollChild:GetHeight() - self:GetHeight(), 0)
-			scrollBar:SetMinMaxValues(0, scrollMax)
-			scrollBar:SetValue(0)
-		end
-
-		local function scrollFrame_OnMouseWheel(self, delta)
-			local scrollBar = self:GetParent().scrollBar
-			local min, max = scrollBar:GetMinMaxValues()
-			local current = scrollBar:GetValue()
-
-			if IsShiftKeyDown() and (delta > 0) then
-			scrollBar:SetValue(min)
-			elseif IsShiftKeyDown() and (delta < 0) then
-			scrollBar:SetValue(max)
-			elseif (delta < 0) and (current < max) then
-			scrollBar:SetValue(current + SCROLL_STEP)
-			elseif (delta > 0) and (current > 1) then
-			scrollBar:SetValue(current - SCROLL_STEP)
-			end
-		end
-
-		function FontSelector:CreateScrollFrame()
-			local scrollFrame = CreateFrame('ScrollFrame', nil, self)
-			scrollFrame:EnableMouseWheel(true)
-			scrollFrame:SetScript('OnSizeChanged', scrollFrame_OnSizeChanged)
-			scrollFrame:SetScript('OnMouseWheel', scrollFrame_OnMouseWheel)
-
-			return scrollFrame
-		end
-	end
-
-	do
-		local function scrollBar_OnValueChanged(self, value)
-			self:GetParent().scrollFrame:SetVerticalScroll(value)
-		end
-
-		function FontSelector:CreateScrollBar()
-			local scrollBar = CreateFrame('Slider', nil, self)
-			scrollBar:SetOrientation('VERTICAL')
-			scrollBar:SetScript('OnValueChanged', scrollBar_OnValueChanged)
-
-			local bg = scrollBar:CreateTexture(nil, 'BACKGROUND')
-			bg:SetAllPoints(true)
-			bg:SetTexture(0, 0, 0, 0.5)
-
-			local thumb = scrollBar:CreateTexture(nil, 'OVERLAY')
-			thumb:SetTexture([[Interface\Buttons\UI-ScrollBar-Knob]])
-			thumb:SetSize(25, 25)
-			scrollBar:SetThumbTexture(thumb)
-
-			return scrollBar
-		end
-	end
-
-	function FontSelector:CreateScrollChild()
-		local scrollChild = CreateFrame('Frame')
-		local f_OnClick = function(f) self:Select(f:GetFontFace()) end
-		local buttons = {}
-
-		local i = 0
-		for _, fontId in ipairs(getFontIDs()) do
-			local font = fetchFont(fontId)
-			if isValidFont(font) then
-				i = i + 1
-				local f = FontButton:New(scrollChild, i % 4 == 0 or (i + 1) % 4 == 0)
-				f:SetFontFace(font):SetText(fontId)
-				f:SetScript('OnClick', f_OnClick)
-
-				if i == 1 then
-					f:SetPoint('TOPLEFT')
-					f:SetPoint('TOPRIGHT', scrollChild, 'TOP', -PADDING/2, 0)
-				elseif i == 2 then
-					f:SetPoint('TOPLEFT', scrollChild, 'TOP', PADDING/2, 0)
-					f:SetPoint('TOPRIGHT')
-				else
-					f:SetPoint('TOPLEFT', buttons[i-2], 'BOTTOMLEFT', 0, -PADDING)
-					f:SetPoint('TOPRIGHT', buttons[i-2], 'BOTTOMRIGHT', 0, -PADDING)
-				end
-
-				tinsert(buttons, f)
-			end
-		end
-
-		scrollChild:SetWidth(self.scrollFrame:GetWidth())
-		scrollChild:SetHeight(ceil(#buttons / 2) * (BUTTON_HEIGHT + PADDING) - PADDING)
-
-		self.buttons = buttons
-		return scrollChild
-	end
-
-
 	function FontSelector:OnShow()
-		self:UpdateSelected()
+		self:Refresh()
 	end
 
-	function FontSelector:Select(value)
+	function FontSelector:OnHide()
+		local button = table.remove(self.buttons)
+		while button do
+			button:Free()
+			button = table.remove(self.buttons)
+		end
+	end
+
+	function FontSelector:OnSelectValue(value)
 		self:SetSavedValue(value)
 		self:UpdateSelected()
+	end
+
+	function FontSelector:Refresh()
+		local width, height = 0, 0
+		local selectedValue = self:GetSavedValue()
+
+		for i, id in Fonts:GetAll() do
+			local button = self:GetOrCreateButton(i)
+
+			button:ClearAllPoints()
+			if i == 1 then
+				button:SetPoint('TOPLEFT')
+			else
+				button:SetPoint('TOPLEFT', self.buttons[i - 1], 'BOTTOMLEFT', 0, -self.buttonPadding)
+			end
+
+			button:SetPoint('RIGHT')
+			button:SetSize(self.buttonWidth, self.buttonHeight)
+			button:SetValue(id, selectedValue)
+			button:Show()
+
+			width = max(width, button:GetWidth())
+		end
+
+		height = (#self.buttons * self.buttons[1]:GetHeight()) + (#self.buttons-1) * self.buttonPadding
+
+		self:GetParent():SetSize(width, height)
+	end
+
+	function FontSelector:GetOrCreateButton(i)
+		local button = self.buttons[i]
+
+		if not button then
+			button = FontButton:New(self)
+			self.buttons[i] = button
+		end
+
+		return button
 	end
 
 	function FontSelector:SetSavedValue(value)
@@ -240,7 +190,7 @@ do
 	function FontSelector:UpdateSelected()
 		local selectedValue = self:GetSavedValue()
 		for i, button in pairs(self.buttons) do
-			button:SetChecked(button:GetFontFace() == selectedValue)
+			button:SetChecked(button:GetAttribute('texture') == selectedValue)
 		end
 	end
 end
