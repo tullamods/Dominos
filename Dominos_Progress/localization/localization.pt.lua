6066df058396d05ce1a3fc5ebf9b5a8679b02724
Jason Greer <tuller@tullamods.com>
2016-07-23 11:57:55 -0400

    add Brazilian Portuguese

---
+++ Dominos_Progress/localization/localization.pt.lua
@@ -0,0 +1,10 @@
+local L = LibStub('AceLocale-3.0'):NewLocale('Dominos-Progress', 'ptBR')
+if not L then return end
+
+L["AlwaysShowText"] = "Sempre Exibir Texto"
+L["CompressValues"] = "Compactar Valores"
+L["Font"] = "Fonte"
+L["Segmented"] = "Segmentado"
+L["ShowLabels"] = "Exibir Marcadores"
+L["Texture"] = "Textura"
+L["Width"] = "Largura"
