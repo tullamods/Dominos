f0157dbb5710e11bd34ceb4b354c46d1962bb9e5
Jason Greer <tuller@tullamods.com>
2017-11-18 23:05:58 -0500

    move fader code to helpers

--- Dominos/core/helpers.lua
+++ Dominos/core/helpers.lua
@@ -1,4 +1,4 @@
--- functions I call at least three times
+-- functions I call at least three(ish) times
 
 local AddonName = ...
 local Addon = _G[AddonName]
@@ -38,4 +38,46 @@ function Addon:CreateNameGenerator(prefix)
 		id = id + 1
 		return ('%s_%s_%d'):format(AddonName, prefix, id)
 	end
+end
+
+-- A functional way to fade a frame from one opacity to another without constantly
+-- creating new animation groups for the frame
+do
+    local faders = {}
+
+    local function fader_OnFinished(self)
+        self:GetParent():SetAlpha(self.toAlpha)
+    end
+
+    local function fader_Create(parent)
+        local animation = parent:CreateAnimationGroup()
+        animation:SetLooping('NONE')
+        animation:SetScript('OnFinished', fader_OnFinished)
+
+        local fade = animation:CreateAnimation('Alpha')
+        fade:SetSmoothing('IN_OUT')
+
+        return function(toAlpha, duration)
+            if animation:IsPlaying() then
+                animation:Pause()
+            end
+
+            fade:SetFromAlpha(parent:GetAlpha())
+            fade:SetToAlpha(toAlpha)
+            fade:SetDuration(duration)
+
+            animation.toAlpha = toAlpha
+            animation:Play()
+        end
+    end
+
+    function Addon:Fade(frame, toAlpha, duration)
+        local fade = faders[frame]
+
+        if not fade then
+            fade = fader_Create(frame)
+        end
+
+        fade(toAlpha, duration)
+    end
 end
