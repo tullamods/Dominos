5d3ef8057682db67cef85bf981f3d4b011bfac2f
Jason Greer <tuller@tullamods.com>
2019-09-21 16:53:01 -0400

    add ability to disable modules

--- Dominos/Dominos.lua
+++ Dominos/Dominos.lua
@@ -15,7 +15,7 @@ function Addon:OnInitialize()
 	self:UpgradeDatabase()
 
 	-- create a loader for the options menu
-	local f = CreateFrame('Frame', nil, _G.InterfaceOptionsFrame)
+	local f = CreateFrame('Frame', nil, InterfaceOptionsFrame)
 	f:SetScript('OnShow', function()
 		f:SetScript('OnShow', nil)
 		LoadAddOn(CONFIG_ADDON_NAME)
@@ -59,7 +59,6 @@ end
 
 -- profile events
 function Addon:OnNewProfile(msg, db, name)
-	self.isNewProfile = true
 	self:Printf(L.ProfileCreated, name)
 end
 
@@ -83,14 +82,19 @@ end
 -- Load is called when the addon is first enabled, and also whenever a profile
 -- is loaded
 function Addon:Load()
-	local module_load = function(module)
-		if module.Load then
-			module:Load()
+	local function module_load(module, id)
+		if not self.db.profile.modules[id] then
+			return
+		end
+
+		local f = module.Load
+		if type(f) == "function"  then
+			f(module)
 		end
 	end
 
-	for _, module in self:IterateModules() do
-		local success, msg = pcall(module_load, module)
+	for id, module in self:IterateModules() do
+		local success, msg = pcall(module_load, module, id)
 		if not success then
 			self:Printf('Failed to load %s\n%s', module:GetName(), msg)
 		end
@@ -102,15 +106,20 @@ end
 
 -- unload is called when we're switching profiles
 function Addon:Unload()
-	local module_unload = function(module)
-		if module.Unload then
-			module:Unload()
+	local function module_unload(module, id)
+		if not self.db.profile.modules[id] then
+			return
+		end
+
+		local f = module.Unload
+		if type(f) == "function"  then
+			f(module)
 		end
 	end
 
 	-- unload any module stuff
-	for _, module in self:IterateModules() do
-		local success, msg = pcall(module_unload, module)
+	for id, module in self:IterateModules() do
+		local success, msg = pcall(module_unload, module, id)
 		if not success then
 			self:Printf('Failed to unload %s\n%s', module:GetName(), msg)
 		end
@@ -163,7 +172,13 @@ function Addon:GetDatabaseDefaults()
 				showgrid = true,
 			},
 
-			frames = {}
+			frames = {},
+
+			-- what modules are enabled
+			-- module[id] = enabled
+			modules = {
+				["**"] = true
+			}
 		}
 	}
 end
@@ -189,7 +204,6 @@ function Addon:SaveProfile(name)
 		self:Unload()
 		self.db:SetProfile(name)
 		self.db:CopyProfile(toCopy)
-		self.isNewProfile = nil
 		self:Load()
 	end
 end
@@ -199,7 +213,6 @@ function Addon:SetProfile(name)
 	if profile and profile ~= self.db:GetCurrentProfile() then
 		self:Unload()
 		self.db:SetProfile(profile)
-		self.isNewProfile = nil
 		self:Load()
 	else
 		self:Printf(L.InvalidProfile, name or 'null')
@@ -219,7 +232,6 @@ function Addon:CopyProfile(name)
 	if name and name ~= self.db:GetCurrentProfile() then
 		self:Unload()
 		self.db:CopyProfile(name)
-		self.isNewProfile = nil
 		self:Load()
 	end
 end
@@ -227,7 +239,6 @@ end
 function Addon:ResetProfile()
 	self:Unload()
 	self.db:ResetProfile()
-	self.isNewProfile = true
 	self:Load()
 end
 
