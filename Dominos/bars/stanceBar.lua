7c5716ea4a51c2c2642ddd65fdcfec4e7ca3e026
Jason Greer <tuller@tullamods.com>
2020-08-16 17:48:40 -0400

    AddQuickBindingSupport + a more clear test for the stance bar

--- Dominos/bars/stanceBar.lua
+++ Dominos/bars/stanceBar.lua
@@ -1,16 +1,24 @@
 -- a bar for displaying class specific buttons for things like stances/forms/etc
-
 local _, Addon = ...
-local playerClass = select(2, UnitClass('player'))
+
+local HAS_STANCE_BAR = {
+	WARRIOR = Addon:IsBuild('classic'),
+	PALADIN = true,
+	HUNTER = false,
+	ROGUE = true,
+	PRIEST = Addon:IsBuild('retail'),
+	DEATHKNIGHT = false,
+	SHAMAN = false,
+	MAGE = false,
+	WARLOCK = false,
+	MONK = false,
+	DRUID = true,
+	DEMONHUNTER = false
+}
 
 -- don't bother loading the module if the player is currently playing something
 -- without a stance
-if not (
-	playerClass == 'DRUID'
-	or playerClass == 'ROGUE'
-	or (not Addon:IsBuild("classic") and playerClass == 'PRIEST')
-	or (Addon:IsBuild("classic") and (playerClass == 'WARRIOR' or playerClass == 'PALADIN'))
-) then
+if not HAS_STANCE_BAR[UnitClassBase('player')] then
 	return
 end
 
@@ -18,66 +26,74 @@ end
 local StanceButtons = {}
 
 for id = 1, NUM_STANCE_SLOTS do
-	local button = _G[('StanceButton%d'):format(id)]
+    local button = _G[('StanceButton%d'):format(id)]
 
-	Addon.BindableButton:Inject(button, 'SHAPESHIFTBUTTON', id)
+    Addon.BindableButton:AddQuickBindingSupport(button, 'SHAPESHIFTBUTTON' .. id)
 
-	StanceButtons[id] = button
+    StanceButtons[id] = button
 end
 
 -- bar
 local StanceBar = Addon:CreateClass('Frame', Addon.ButtonBar)
 
 function StanceBar:New()
-	return StanceBar.proto.New(self, 'class')
+    return StanceBar.proto.New(self, 'class')
 end
 
 function StanceBar:GetDefaults()
-	return {
-		point = 'CENTER',
-		spacing = 2
-	}
+    return {
+        point = 'CENTER',
+        spacing = 2
+    }
 end
 
 function StanceBar:NumButtons()
-	return GetNumShapeshiftForms() or 0
+    return GetNumShapeshiftForms() or 0
 end
 
 function StanceBar:AcquireButton(index)
-	return StanceButtons[index]
+    return StanceButtons[index]
 end
 
 function StanceBar:OnAttachButton(button)
-	Addon:GetModule('ButtonThemer'):Register(button, 'Class Bar')
-	Addon:GetModule('Tooltips'):Register(button)
+    button:UpdateHotkeys()
+    Addon:GetModule('ButtonThemer'):Register(button, 'Class Bar')
+    Addon:GetModule('Tooltips'):Register(button)
 end
 
 function StanceBar:OnDetachButton(button)
-	Addon:GetModule('ButtonThemer'):Unregister(button, 'Class Bar')
-	Addon:GetModule('Tooltips'):Unregister(button)
+    Addon:GetModule('ButtonThemer'):Unregister(button, 'Class Bar')
+    Addon:GetModule('Tooltips'):Unregister(button)
 end
 
 -- module
 local StanceBarModule = Addon:NewModule('StanceBar', 'AceEvent-3.0')
 
 function StanceBarModule:Load()
-	self.bar = StanceBar:New()
+    self.bar = StanceBar:New()
 
-	self:RegisterEvent('UPDATE_SHAPESHIFT_FORMS', 'UpdateNumForms')
-	self:RegisterEvent('PLAYER_REGEN_ENABLED', 'UpdateNumForms')
-	self:RegisterEvent('PLAYER_ENTERING_WORLD', 'UpdateNumForms')
+    self:RegisterEvent('UPDATE_SHAPESHIFT_FORMS', 'UpdateNumForms')
+    self:RegisterEvent('PLAYER_REGEN_ENABLED', 'UpdateNumForms')
+    self:RegisterEvent('PLAYER_ENTERING_WORLD', 'UpdateNumForms')
+    self:RegisterEvent('UPDATE_BINDINGS')
 end
 
 function StanceBarModule:Unload()
-	self:UnregisterAllEvents()
+    self:UnregisterAllEvents()
 
-	if self.bar then
-		self.bar:Free()
-	end
+    if self.bar then
+        self.bar:Free()
+    end
 end
 
 function StanceBarModule:UpdateNumForms()
-	if InCombatLockdown() then return end
+    if InCombatLockdown() then
+        return
+    end
 
-	self.bar:UpdateNumButtons()
+    self.bar:UpdateNumButtons()
+end
+
+function StanceBarModule:UPDATE_BINDINGS()
+    self.bar:ForButtons('UpdateHotkeys')
 end
