8632263d491fa715449bd61da74c64d13fe31a67
Wang Hong <wanghong@superred.com.cn>
2020-10-24 13:15:09 +0800

    Update zhCN translation

--- Dominos_Progress/localization/localization.cn.lua
+++ Dominos_Progress/localization/localization.cn.lua
@@ -8,7 +8,30 @@
 local L = LibStub('AceLocale-3.0'):NewLocale('Dominos-Progress', 'zhCN')
 if not L then return end
 
+L.OneBarMode = '只使用一条进度条'
+L.Progress = '进度条'
 L.Texture = '材质'
 L.Width = '宽'
 L.Height = '高'
 L.AlwaysShowText = '总显示文本'
+L.Segmented = '分割'
+L.Font = '字体'
+L.AutoSwitchModes = '自动切换模式'
+L.Display_label = '显示标签'
+L.Display_value = '显示当前数值'
+L.Display_max = '显示最大数值'
+L.Display_bonus = '显示休息/额外经验值'
+L.Display_percent = '显示百分比'
+L.Display_remaining = '显示剩余经验值'
+L.CompressValues = '显示简短的数值'
+
+L.Color_xp = '经验条颜色'
+L.Color_xp_bonus = '额外经验条颜色'
+L.Color_honor = '荣誉条颜色'
+L.Color_honor_bonus = '额外荣誉条颜色'
+L.Color_artifact = '神器能量条颜色'
+L.Color_azerite = '艾泽里特条颜色'
+
+L.Paragon = '阵营'
+L.Azerite = '艾泽里特能量'
+L.SkipInactiveModes = "忽略未激活进度条"
