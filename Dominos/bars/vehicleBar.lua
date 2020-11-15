1a8bb5785750a0f2ab8a7483fe917d049f43b7f6
Jason Greer <tuller@tullamods.com>
2020-11-15 16:50:22 -0500

    fix typo

--- Dominos/bars/vehicleBar.lua
+++ Dominos/bars/vehicleBar.lua
@@ -16,7 +16,7 @@ end
 local VehicleBar = Addon:CreateClass('Frame', Addon.Frame)
 
 function VehicleBar:New()
-    local bar = VehicleBar.proto.New(self, vehicle)
+    local bar = VehicleBar.proto.New(self, "vehicle")
 
     -- drop need for showstates for this case
     if bar:GetShowStates() == '[@vehicle,exists]show;hide' then
