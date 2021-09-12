564ca223490750c909e50916497680d09363ec73
Willian Krueger <wkrueger128@gmail.com>
2021-09-12 16:25:09 -0300

    cleanup

--- Dominos_Config/options/profiles.lua
+++ Dominos_Config/options/profiles.lua
@@ -1,8 +1,8 @@
 local _, Addon = ...
 
 local DualSpec = _G.LibStub("LibDualSpec-1.0")
-local options = _G.LibStub("AceDBOptions-3.0"):GetOptionsTable(Addon:GetParent().db, true)
 local db = Addon:GetParent().db
+local options = _G.LibStub("AceDBOptions-3.0"):GetOptionsTable(db, true)
 DualSpec:EnhanceOptions(options, db)
 
 Addon:AddOptionsPanelOptions("profiles", options)
