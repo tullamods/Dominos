f60d5e78515e5060982e3d3d5cf8a82ab133ac6e
Jason Greer <tuller@tullamods.com>
2022-10-02 18:09:22 -0400

    fine, i'll make my own stance bar

--- Dominos/bars/stanceBar.lua
+++ Dominos/bars/stanceBar.lua
@@ -29,8 +29,25 @@ end
 -- Button setup
 --------------------------------------------------------------------------------
 
-local function getStanceButton(id)
-    return _G[('StanceButton%d'):format(id)]
+local function getOrCreateStanceButton(id)
+    local name = ('%sStanceButton%d'):format(AddonName, id)
+
+    local button = _G[name]
+    if button then
+        return button
+    end
+
+    button = CreateFrame('CheckButton', name, nil, 'StanceButtonTemplate')
+    button.commandName = ('SHAPESHIFTBUTTON%d'):format(id)
+    button:SetID(id)
+    button:Hide()
+    button:EnableMouseWheel(true)
+    button.cooldown:SetDrawEdge(false)
+    button:SetScript("OnUpdate", nil)
+
+    Mixin(button, Addon.BindableButton)
+
+    return button
 end
 
 --------------------------------------------------------------------------------
@@ -59,11 +76,10 @@ function StanceBar:NumButtons()
 end
 
 function StanceBar:AcquireButton(index)
-    return getStanceButton(index)
+    return getOrCreateStanceButton(index)
 end
 
 function StanceBar:OnAttachButton(button)
-    -- button:UpdateHotkeys()
     Addon:GetModule('ButtonThemer'):Register(button, 'Class Bar')
     Addon:GetModule('Tooltips'):Register(button)
 end
@@ -85,10 +101,18 @@ local StanceBarModule = Addon:NewModule('StanceBar', 'AceEvent-3.0')
 function StanceBarModule:Load()
     self.bar = StanceBar:New()
 
-    self:RegisterEvent('UPDATE_SHAPESHIFT_FORMS', 'UpdateNumForms')
-    self:RegisterEvent('PLAYER_REGEN_ENABLED', 'UpdateNumForms')
-    self:RegisterEvent('PLAYER_ENTERING_WORLD', 'UpdateNumForms')
-    self:RegisterEvent('UPDATE_BINDINGS')
+    -- self:RegisterEvent("ACTIONBAR_PAGE_CHANGED")
+    self:RegisterEvent("PLAYER_ENTERING_WORLD", 'UpdateNumForms')
+    self:RegisterEvent("PLAYER_REGEN_ENABLED", 'UpdateNumForms')
+    self:RegisterEvent("UPDATE_BINDINGS")
+    -- self:RegisterEvent("UPDATE_BONUS_ACTIONBAR", 'UpdateStanceButtons')
+    -- self:RegisterEvent("UPDATE_OVERRIDE_ACTIONBAR", 'UpdateStanceButtons')
+    -- self:RegisterEvent("UPDATE_POSSESS_BAR", 'UpdateStanceButtons')
+    self:RegisterEvent("UPDATE_SHAPESHIFT_FORM", 'UpdateStanceButtons')
+    -- self:RegisterEvent("UPDATE_SHAPESHIFT_FORMS", 'UpdateStanceButtons')
+    self:RegisterEvent("UPDATE_SHAPESHIFT_USABLE", 'UpdateStanceButtons')
+    -- self:RegisterEvent("UPDATE_VEHICLE_ACTIONBAR", 'UpdateStanceButtons')
+    self:RegisterEvent("UPDATE_SHAPESHIFT_COOLDOWN", 'UpdateStanceButtons')
 end
 
 function StanceBarModule:Unload()
@@ -100,13 +124,45 @@ function StanceBarModule:Unload()
 end
 
 function StanceBarModule:UpdateNumForms()
-    if InCombatLockdown() then
-        return
+    if not InCombatLockdown() then
+        self.bar:UpdateNumButtons()
     end
 
-    self.bar:UpdateNumButtons()
+    self:UpdateStanceButtons()
 end
 
 function StanceBarModule:UPDATE_BINDINGS()
     self.bar:ForButtons('UpdateHotkeys')
 end
+
+StanceBarModule.UpdateStanceButtons = Addon:Defer(function(self)
+    local bar = self.bar
+    if not bar then
+        return
+    end
+
+	for i, button in pairs(bar.buttons) do
+        local texture, isActive, isCastable = GetShapeshiftFormInfo(i)
+
+        button:SetAlpha(texture and 1 or 0)
+
+        local icon = button.icon
+
+        icon:SetTexture(texture)
+
+        if isCastable then
+            icon:SetVertexColor(1.0, 1.0, 1.0)
+        else
+            icon:SetVertexColor(0.4, 0.4, 0.4)
+        end
+
+        local start, duration, enable = GetShapeshiftFormCooldown(i)
+        if enable and enable ~= 0 and start > 0 and duration > 0 then
+            button.cooldown:SetCooldown(start, duration)
+        else
+            button.cooldown:Clear()
+        end
+
+        button:SetChecked(isActive and true)
+    end
+end, 0.01, StanceBarModule)
--- Dominos/core/blizzardHider.lua
+++ Dominos/core/blizzardHider.lua
@@ -81,8 +81,18 @@ for i = 1, NUM_ACTIONBAR_BUTTONS do
     disableActionButton(('MultiBarBottomLeftButton%d'):format(i))
 end
 
+if StanceBar and StanceBar.actionButtons then
+    for _, button in pairs(StanceBar.actionButtons) do
+        disableActionButton(button:GetName())
+    end
+
+    Addon:Banish(StanceBar, true)
+
+    ActionBarController:UnregisterEvent('UPDATE_SHAPESHIFT_FORM')
+    ActionBarController:UnregisterEvent('UPDATE_SHAPESHIFT_FORMS')
+    ActionBarController:UnregisterEvent('UPDATE_SHAPESHIFT_USABLE')
+    ActionBarController:UnregisterEvent('UPDATE_INVENTORY_ALERTS') --Wha? indeed
+end
+
 -- disable some action bar controller updates that we probably don't need
--- ActionBarController:UnregisterEvent('UPDATE_SHAPESHIFT_FORM')
--- ActionBarController:UnregisterEvent('UPDATE_SHAPESHIFT_FORMS')
--- ActionBarController:UnregisterEvent('UPDATE_SHAPESHIFT_USABLE')
 -- ActionBarController:UnregisterEvent('UPDATE_POSSESS_BAR')
