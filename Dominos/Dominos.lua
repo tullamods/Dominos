577f7b5b19177ac58f8aee0679c3edb31e40ac9b
Jason Greer <tuller@tullamods.com>
2024-01-28 17:47:23 -0500

    fire event on SetUseOverrideUI

--- Dominos/Dominos.lua
+++ Dominos/Dominos.lua
@@ -702,6 +702,7 @@ end
 -- override ui
 function Addon:SetUseOverrideUI(enable)
     self.db.profile.useOverrideUI = enable and true
+    self.callbacks:Fire("USE_OVERRRIDE_UI_CHANGED", self:UsingOverrideUI())
     self:UpdateUseOverrideUI()
 end
 
