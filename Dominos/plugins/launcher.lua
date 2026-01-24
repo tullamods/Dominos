6a5ea74d09b2746278e46e91015f2e9d77a82d34
Jason Greer <tuller@tullamods.com>
2026-01-24 09:33:52 -0500

    Add latency to TBC At some point I should adjust Zed's auto formatter if I continue using it.

--- Dominos/plugins/launcher.lua
+++ Dominos/plugins/launcher.lua
@@ -8,7 +8,7 @@ function Launcher:OnInitialize()
     DBIcon:Register(AddonName, self:CreateDataBrokerObject(), self:GetSettings())
 
     if AddonCompartmentFrame and AddonCompartmentFrame.RegisterAddon then
-        AddonCompartmentFrame:RegisterAddon{
+        AddonCompartmentFrame:RegisterAddon {
             text = AddonName,
             icon = C_AddOns.GetAddOnMetadata(AddonName, 'IconTexture'),
             -- unfortunately, func doesn't pass in what button was clicked here
@@ -33,48 +33,46 @@ function Launcher:GetSettings()
 end
 
 function Launcher:CreateDataBrokerObject()
-    return LibStub('LibDataBroker-1.1'):NewDataObject(
-        AddonName,
-        {
-            type = 'launcher',
-            icon = C_AddOns.GetAddOnMetadata(AddonName, 'IconTexture'),
-            OnClick = Addon.OnLaunch,
-            OnTooltipShow = function(tooltip)
-                if not tooltip or not tooltip.AddLine then
-                    return
-                end
-
-                GameTooltip_SetTitle(tooltip, AddonName)
-
-                if Addon:Locked() then
-                    GameTooltip_AddInstructionLine(tooltip, L.ConfigEnterTip)
-                else
-                    GameTooltip_AddInstructionLine(tooltip, L.ConfigExitTip)
-                end
-
-                if Addon:IsBindingModeEnabled() then
-                    GameTooltip_AddInstructionLine(tooltip, L.BindingExitTip)
-                else
-                    GameTooltip_AddInstructionLine(tooltip, L.BindingEnterTip)
-                end
-
-                if Addon:IsConfigAddonEnabled() then
-                    GameTooltip_AddInstructionLine(tooltip, L.ShowOptionsTip)
-                end
-
-                if Addon:IsBuild('mists', 'cata', 'wrath') then
-                    local _, _, latencyHome, latencyWorld = GetNetStats()
-
-                    GameTooltip_AddBlankLinesToTooltip(tooltip, 1)
-                    GameTooltip_AddNormalLine(tooltip, MAINMENUBAR_LATENCY_LABEL:format(latencyHome, latencyWorld))
-                elseif Addon:IsBuild('vanilla') then
-                    local _,_, latencyHome, latencyWorld = GetNetStats()
-                    local latency = math.max(latencyHome, latencyWorld)
-
-                    GameTooltip_AddBlankLinesToTooltip(tooltip, 1)
-                    GameTooltip_AddNormalLine(tooltip, ("%s %d%s"):format(MAINMENUBAR_LATENCY_LABEL, latency, MILLISECONDS_ABBR))
-                end
+    return LibStub('LibDataBroker-1.1'):NewDataObject(AddonName, {
+        type = 'launcher',
+        icon = C_AddOns.GetAddOnMetadata(AddonName, 'IconTexture'),
+        OnClick = Addon.OnLaunch,
+        OnTooltipShow = function(tooltip)
+            if not tooltip or not tooltip.AddLine then
+                return
             end
-        }
-    )
+
+            GameTooltip_SetTitle(tooltip, AddonName)
+
+            if Addon:Locked() then
+                GameTooltip_AddInstructionLine(tooltip, L.ConfigEnterTip)
+            else
+                GameTooltip_AddInstructionLine(tooltip, L.ConfigExitTip)
+            end
+
+            if Addon:IsBindingModeEnabled() then
+                GameTooltip_AddInstructionLine(tooltip, L.BindingExitTip)
+            else
+                GameTooltip_AddInstructionLine(tooltip, L.BindingEnterTip)
+            end
+
+            if Addon:IsConfigAddonEnabled() then
+                GameTooltip_AddInstructionLine(tooltip, L.ShowOptionsTip)
+            end
+
+            if Addon:IsBuild('mists', 'cata', 'wrath') then
+                local _, _, latencyHome, latencyWorld = GetNetStats()
+
+                GameTooltip_AddBlankLinesToTooltip(tooltip, 1)
+                GameTooltip_AddNormalLine(tooltip, MAINMENUBAR_LATENCY_LABEL:format(latencyHome, latencyWorld))
+            elseif Addon:IsBuild('vanilla', 'tbc') then
+                local _, _, latencyHome, latencyWorld = GetNetStats()
+                local latency = math.max(latencyHome, latencyWorld)
+                local latencyText = ("%s %d%s"):format(MAINMENUBAR_LATENCY_LABEL, latency, MILLISECONDS_ABBR)
+
+                GameTooltip_AddBlankLinesToTooltip(tooltip, 1)
+                GameTooltip_AddNormalLine(tooltip, latencyText)
+            end
+        end
+    })
 end
